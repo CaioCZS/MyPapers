91294a0557c8cae49608a2b45d61a6864d7964e9
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-28 18:56:02 -0300

    HomePage buttons

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,23 @@
 import { ContainerHeader, MenuIcon, CartIcon } from "./style";
-import logo from "../../assets/img/logo_v2_white.png"
+import logo from "../../assets/img/logo_v2_white.png";
+import { useNavigate } from "react-router-dom";
 
 export default function Header() {
+  const navigate = useNavigate();
+
+  function Page(page) {
+    if (page === "cart") {
+      navigate("/carrinho");
+    } else if (page === "logo") {
+      navigate("/");
+    }
+  }
+
   return (
     <ContainerHeader>
       <MenuIcon />
-      <img alt="logo" src= {logo} />
-      <CartIcon />
+      <img alt="logo" src={logo} onClick={() => Page("logo")}  />
+      <CartIcon onClick={() => Page('cart')} />
     </ContainerHeader>
   );
 }
--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,38 +1,29 @@
 import Header from "../../components/Header";
 import { ContainerBody, ContainerAd, ContainerProduct, Product } from "./style";
 import Search from "../../components/Search/index";
+import { useNavigate } from "react-router-dom";
 
 export default function HomePage() {
+  const navigate = useNavigate();
+
+  function Page(page) {
+    if (page === "product") {
+      navigate("/item");
+    } else if (page === "ad") {
+      navigate("/");
+    }
+  }
+
   return (
     <>
       <Header />
       <ContainerBody>
         <Search />
         <ContainerAd>
-          <img alt="ad" src="" />
+          <img alt="ad" src="" onClick={() => Page("ad")} />
         </ContainerAd>
         <ContainerProduct>
-          <Product>
-            <img alt="product" src=""></img>
-            <h3>Name preço</h3>
-          </Product>
-          <Product>
-            <img alt="product" src=""></img>
-            <h3>Name preço</h3>
-          </Product>
-          <Product>
-            <img alt="product" src=""></img>
-            <h3>Name preço</h3>
-          </Product>
-          <Product>
-            <img alt="product" src=""></img>
-            <h3>Name preço</h3>
-          </Product>
-          <Product>
-            <img alt="product" src=""></img>
-            <h3>Name preço</h3>
-          </Product>
-          <Product>
+          <Product onClick={() => Page("product")}>
             <img alt="product" src=""></img>
             <h3>Name preço</h3>
           </Product>
