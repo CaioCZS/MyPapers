135c7e7df9e36e36bb4aeda5178d95f58803607a
Letícia Montenegro <montenegro.letiicia@gmail.com>
2023-04-28 19:37:35 -0300

    getItens

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -2,9 +2,21 @@ import Header from "../../components/Header";
 import { ContainerBody, ContainerAd, ContainerProduct, Product } from "./style";
 import Search from "../../components/Search/index";
 import { useNavigate } from "react-router-dom";
+import productApi from "../../services/productApi";
+import { useEffect, useState } from "react";
 
 export default function HomePage() {
+  const [itens, setItens] = useState();
   const navigate = useNavigate();
+
+  useEffect(getItems, []);
+
+  function getItems() {
+    productApi
+      .itens()
+      .then((res) => setItens(res.data))
+      .catch((err) => alert(err.response.data));
+  }
 
   function Page(page) {
     if (page === "product") {
@@ -23,10 +35,13 @@ export default function HomePage() {
           <img alt="ad" src="" onClick={() => Page("ad")} />
         </ContainerAd>
         <ContainerProduct>
-          <Product onClick={() => Page("product")}>
-            <img alt="product" src=""></img>
-            <h3>Name preço</h3>
-          </Product>
+          {itens &&
+            itens.map((p) => (
+              <Product key={p._id} onClick={() => Page("product")}>
+                <img alt="product" src={p.image}></img>
+                <h3>Name preço</h3>
+              </Product>
+            ))}
         </ContainerProduct>
       </ContainerBody>
     </>
